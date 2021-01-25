20a22deef5ad0f2c147a597dd76fc793da4ef00c
amcc <alistairmcclymont@gmail.com>
2021-01-25 23:38:27 +0000

    feat: use json import

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 let step = 0;
 let path = 0;
 let dotSize = 1;
-let colour = 200;
-let alpha = 0.2;
+let colour = 0;
+let saturation = 0;
+let bigSaturation = 50;
+let brightness = 100;
+let bigAlpha = 0.15;
+let littleAlpha = 0.2;
 
 let xPadding = 0;
 let yPadding = 0;
-const maxWidth = 1000;
-const maxHeight = 1000;
+const yShift = -900;
+const xShift = -300;
+const maxWidth = 500;
+const maxHeight = 500;
 let scale = 1;
 let speed = 10;
+let paths;
+
+function preload() {
+  // Get the most recent earthquake in the database
+  let url = "./paths/allsofar.json";
+  data = loadJSON(url, "json");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,33 +33,38 @@ function setup() {
   background(0, 0, 6);
   noStroke();
   frameRate(60);
+  paths = shuffle(data.paths);
+  console.log("paths", paths);
 }
 
 function draw() {
-  fill(colour, 50, 100, alpha);
-  if (movement[path]) {
-    if (step <= movement[path].length - speed) {
-      if (movement[path][step])
+  if (paths[path]) {
+    if (step <= paths[path].points.length - speed) {
+      if (paths[path].points[step])
         for (let i = 0; i < speed; i++) {
+          fill(colour, bigSaturation, brightness, bigAlpha);
           circle(
-            xPadding + movement[path][step][0] * scale,
-            yPadding + movement[path][step][1] * scale,
+            xPadding + paths[path].points[step][0] * scale + xShift,
+            yPadding + paths[path].points[step][1] * scale + yShift,
+            dotSize * 3
+          );
+          fill(colour, saturation, brightness, littleAlpha);
+          circle(
+            xPadding + paths[path].points[step][0] * scale + xShift,
+            yPadding + paths[path].points[step][1] * scale + yShift,
             dotSize
           );
           step++;
+          colour += 360 / paths[path].points.length;
         }
       // console.log("step ", step);
     } else {
-      console.log(
-        "path is ",
-        path,
-        " and movement length is ",
-        movement.length
-      );
-      if (path <= movement.length) {
+      console.log("path is ", path, " and movement length is ", paths.length);
+      if (path <= paths.length) {
         step = 0;
+        colour = 0;
         path++;
-        colour += path * 5;
+        // colour += path * 2;
         console.log("colour", colour);
         console.log("path", path);
       }
@@ -57,3 +75,23 @@ function draw() {
 // function windowResized() {
 //   resizeCanvas(windowWidth, windowHeight);
 // }
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
